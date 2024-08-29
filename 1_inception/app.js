06d548bb4e58a7085cb819a23f48afd5d2f65fd8
srinivasch0306 <srinivasch2202@gmail.com>
2024-08-29 15:16:09 +0530

    learning

--- 1_inception/app.js
+++ 1_inception/app.js
@@ -1,3 +1,30 @@
-const heading=React.createElement("h1",{},"helo react");
-const root=ReactDOM.createRoot(document.getElementById("root"));
+const heading = React.createElement("h1", { id: "heading" }, "helo react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+
+// console.log(heading);
+
+/*
+ *  <div id="parent">
+ *          <div id="child">
+ *                <h1></h1>
+ *          </div>
+ *  </div>
+ *
+ */
+
+const head = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    // if u need to create multiple siblings we have to create a array
+    
+      React.createElement("h1", {}, "i am h1 tg")
+    
+  )
+);
+
+const root_ = ReactDOM.createRoot(document.getElementById("parent"));
+root_.render(head);
