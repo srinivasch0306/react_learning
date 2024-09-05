691bed77a96fbb92e9368e845c717c75cd562636
srinivasch0306 <srinivasch2202@gmail.com>
2024-09-05 10:16:47 +0530

    learning

--- 1_inception/app.js
+++ 1_inception/app.js
@@ -1,33 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom"
 
-const heading = React.createElement("h1", { id: "heading" }, "helo react");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// const heading = React.createElement("h1", { id: "heading" }, "helo react");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
 
-// console.log(heading);
+// // console.log(heading);
 
-/*
- *  <div id="parent">
- *          <div id="child">
- *                <h1></h1>
- *          </div>
- *  </div>
- *
- */
+// /*
+//  *  <div id="parent">
+//  *          <div id="child">
+//  *                <h1></h1>
+//  *          </div>
+//  *  </div>
+//  *
+//  */
 
-const head = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    // if u need to create multiple siblings we have to create a array
+// const head = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     // if u need to create multiple siblings we have to create a array
     
-      React.createElement("h1", {}, "i am h1 hello tg")
+//       React.createElement("h1", {}, "i am h1 hello tg")
     
-  )
-);
+//   )
+// );
 
-const root_ = ReactDOM.createRoot(document.getElementById("parent"));
-root_.render(head);
+// const root_ = ReactDOM.createRoot(document.getElementById("parent"));
+// root_.render(head);
+
+
+
+
---
+++ 3_revise/script.js
@@ -0,0 +1,48 @@
+// const heading=document.createElement("h1");
+// heading.innerHTML="hello";
+
+// const root=document.getElementById("root");
+// root.appendChild(heading);
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// jsk heading
+
+const jskheading = <h1 className="heading"> namaste javascript</h1>;
+
+const Title = () => (
+  // we can use return and sometimes
+  <h1 className="head">namaste react functional companent </h1>
+);
+
+const Fn = () => <h1>hey whatsapp</h1>;
+
+const element = <em>hello </em>;
+
+const va_r = 10000;
+
+const Learning = () => (
+  <div id="container">
+    <Title />
+    {element}
+    {va_r}
+    <Fn />
+    <h2>{2 - 3}</h2>
+    <h1 className="JSK">hello world jsk</h1>
+  </div>
+);
+
+// const Learning = () => (
+//   <div id="container">
+//     <h1 className="JSK">Hello World JSK</h1>
+//   </div>
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(<Learning />);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Learning />);
